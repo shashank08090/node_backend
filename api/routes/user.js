082c14d4b5e8db2e06bb6414649c8dfec7f3856c
shash08090 <shashank08090@gmail.com>
2021-06-03 00:58:54 +0530

    added login functionality, hence API is complete

--- api/routes/user.js
+++ api/routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const mongoose = require('mongoose')
 const bcrypt=require('bcrypt')
 const User = require('../model/user')
+const jwt = require('jsonwebtoken')
 
 router.get('/',(req,res,next)=>{
     res.status(200).json({
@@ -10,7 +11,7 @@ router.get('/',(req,res,next)=>{
     })
 })
 
-router.post('/',(req,res,next)=>{
+router.post('/signup',(req,res,next)=>{
 bcrypt.hash(req.body.password,10,(err,hash)=>{
     if(err){
         return res.status(500).json({
@@ -40,7 +41,40 @@ bcrypt.hash(req.body.password,10,(err,hash)=>{
 })
 })
 
+router.post('/login',(req,res,next)=>{
+    User.find({name:req.body.name}).exec().then(user=>{
+        if(user.length<1){
+            return res.status(401).json({
+                "message":"user does not exist"
+            })
+        }
+        bcrypt.compare(req.body.password,user[0].password,(err,result)=>{
+            if(!result){
+                return res.status(401).json({
+                    "message":"password does not match"
+                })
+            }
+            if(result){
+                const token = jwt.sign({
+                    name:user[0].name,
+                    email:user[0].email
 
+                },"XXsecretkeyXX",{
+                    expiresIn:"24h"
+                })
+                res.status(200).json({
+                    "username":user[0].username,
+                    email:user[0].email,
+                    token:token
+                })
+            }
+        })
+    }).catch(err=>{
+        res.status(500).json({
+            "message":"err"
+        })
+    })
+})
 
 
 
